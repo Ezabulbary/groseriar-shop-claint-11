12146af422928b395c06165da924f1910d890062
Ezabulbary <ezabul.bary@gmail.com>
2022-05-18 12:12:28 +0600

    component added

---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import About from '../About/About';
+import Bannar from '../Bannar/Bannar';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This is home</h2>
+            <Bannar></Bannar>
+            <About></About>
+        </div>
+    );
+};
+
+export default Home;
