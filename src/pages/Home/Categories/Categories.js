c8d1e4377200062015784f20c08a3f720eb0d2cf
Ezabulbary <ezabul.bary@gmail.com>
2022-05-18 20:58:00 +0600

    customlink added my navbar

--- src/pages/Home/Categories/Categories.js
+++ src/pages/Home/Categories/Categories.js
@@ -4,6 +4,7 @@ const Categories = () => {
     return (
         <div className='text-center my-2'>
             <h2>Categories</h2>
+            <h4>Get Your Product from Category</h4>
         </div>
     );
 };
--- src/sheard/Header/Header.js
+++ src/sheard/Header/Header.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Logo from '../../images/logo/logo.jpg';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" sticky="top" bg="light" variant="light">
             <Container>
-                <Navbar.Brand as={Link} to='/home'>
+                <Navbar.Brand as={CustomLink} to='/home'>
                     <img src={Logo} alt="logoImage" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to='/home'>Home</Nav.Link>
-                        <Nav.Link as={Link} to='/blogs'>Blogs</Nav.Link>
-                        <Nav.Link as={Link} to='/about'>About US</Nav.Link>
+                        <Nav.Link as={CustomLink} to='/home'>Home</Nav.Link>
+                        <Nav.Link as={CustomLink} to='/blogs'>Blogs</Nav.Link>
+                        <Nav.Link as={CustomLink} to='/about'>About US</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link as={Link} to='/manageItems'>Manage Items</Nav.Link>
-                        <Nav.Link as={Link} to='/addItems'>Add Items</Nav.Link>
-                        <Nav.Link as={Link} to='/myItems'>My Items</Nav.Link>
-                        <Nav.Link as={Link} to='/login'>Login</Nav.Link>
+                        <Nav.Link as={CustomLink} to='/manageItems'>Manage Items</Nav.Link>
+                        <Nav.Link as={CustomLink} to='/addItems'>Add Items</Nav.Link>
+                        <Nav.Link as={CustomLink} to='/myItems'>My Items</Nav.Link>
+                        <Nav.Link as={CustomLink} to='/login'>Login</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
+            <Outlet />
         </Navbar>
     );
 };
