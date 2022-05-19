b5ab75f9aa3fe55b55232aa838c172b246df658d
Ezabulbary <ezabul.bary@gmail.com>
2022-05-19 19:17:38 +0600

    signout button and privet route added in heder section

--- src/sheard/Header/Header.js
+++ src/sheard/Header/Header.js
@@ -1,10 +1,24 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
 import Logo from '../../images/logo/logo.jpg';
 import CustomLink from '../CustomLink/CustomLink';
+import Loading from '../Loading/Loading';
+import './Header.css';
 
 const Header = () => {
+    const [user, loading] = useAuthState(auth);
+    console.log(user)
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+    const logout = () => {
+        signOut(auth);
+    };
     return (
         <Navbar collapseOnSelect expand="lg" sticky="top" bg="light" variant="light">
             <Container>
@@ -19,10 +33,23 @@ const Header = () => {
                         <Nav.Link as={CustomLink} to='/about'>About US</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link as={CustomLink} to='/manageItems'>Manage Items</Nav.Link>
-                        <Nav.Link as={CustomLink} to='/addItems'>Add Items</Nav.Link>
-                        <Nav.Link as={CustomLink} to='/myItems'>My Items</Nav.Link>
-                        <Nav.Link as={CustomLink} to='/login'>Login</Nav.Link>
+                        {
+                            user?.uid
+                            &&
+                            <>
+                                <Nav.Link as={CustomLink} to='/manageItems'>Manage Items</Nav.Link>
+                                <Nav.Link as={CustomLink} to='/addItems'>Add Items</Nav.Link>
+                                <Nav.Link as={CustomLink} to='/myItems'>My Items</Nav.Link>
+                            </>
+                        }
+                        {
+                            user?.uid
+                                ?
+                                <button onClick={logout} className='bg-light btn logout-btn'>Logout</button>
+
+                                :
+                                <Nav.Link as={CustomLink} to='/login'>Login</Nav.Link>
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
