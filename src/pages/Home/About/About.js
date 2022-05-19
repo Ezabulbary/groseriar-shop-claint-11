6a4ddeaa8fef4cc05b712b5f858bf41ee81ea39e
Ezabulbary <ezabul.bary@gmail.com>
2022-05-20 01:03:34 +0600

    contact page added

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -3,28 +3,29 @@ import AboutImg from '../../../images/about/about.png';
 import { useNavigate, useParams } from 'react-router-dom';
 
 const About = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const navigate = useNavigate();
 
     const getAbout = () => {
         navigate('/about')
     }
-    
+
     return (
         <div className='text-center py-5 rounded' style={{ background: '#FCE6DF' }}>
+            <h2>About Us</h2>
             <div className='d-xl-flex align-items-center justify-content-center'>
-                <div>
-                    <h2>About Us</h2>
-                    <p className='p-2'>
+                <div className='p-4'>
+                    <p className='p-2' style={{ textAlign: 'justify' }}>
                         A warehouse web is a media for storing goods. warehouse web is used by manufacturers, importers, exporters, wholesalers, transport businesses, customs, etc. All the products that are available here are grocery stock, spice warehouse, fruits warehouse, books, perfumes, dresses, gym equipment, bikes, electronics (TV, fridge, microwave, laptop, computer, smartphone, etc.), furniture, hiking gears, sports gear, outdoor camping gears, Vehicle/fleet inventory (car, truck, motorbike, bicycle), toys, etc.
-
+                    </p>
+                    <p className='p-2' style={{ textAlign: 'justify' }}>
                         A warehouse web is an activity of electronically buying or selling
                         wholesale products on online services or over the Internet. warehouse draws on technologies such as mobile commerce, electronic funds transfer, supply chain management, Internet marketing, online transaction processing, electronic data interchange (EDI), and inventory management systems. The warehouse is in turn driven by the technological advances of the semiconductor industry and is the largest sector of the electronics industry.
                     </p>
                     <button onClick={() => getAbout(id)} className='btn btn-success m-4'>About More</button>
                 </div>
                 <img
-                    className='rounded img-fluid '
+                    className='rounded img-fluid pe-4'
                     src={AboutImg}
                     alt="aboutImage"
                 />
--- src/pages/Home/Contact/Contact.js
+++ src/pages/Home/Contact/Contact.js
@@ -1,9 +1,31 @@
+import { faClock, faEnvelope, faLocationDot, faMapLocationDot, faPhone, } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Contact.css';
 
 const Contact = () => {
     return (
-        <div className='text-center my-2 my-contact'>
-            <h2>Contact Us</h2>
+        <div className='row mt-5'>
+            <div className='col-12 col-lg-6 contact-section'>
+                <h1 className='text-success'>CONTACT US</h1>
+                <p><FontAwesomeIcon className='font-icon' icon={faPhone} />+1 650-444-0000</p>
+                <p><Link to='/mailto:makeup@ovon.com'><FontAwesomeIcon className='font-icon' icon={faEnvelope} />makeup@ovon.com</Link></p>
+                <p><FontAwesomeIcon className='font-icon' icon={faLocationDot} />525 West Ave, CT 06850 Norwalk</p>
+                <p><FontAwesomeIcon className='font-icon' icon={faClock} />Mon-Fri: 08.00 - 19.00, Sunday: Closed</p>
+                <p><Link to='/'><FontAwesomeIcon className='font-icon' icon={faMapLocationDot} />Google Maps</Link></p>
+            </div>
+            <div className='col-12 col-lg-6 input-section'>
+                <h3 className='text-success'>GET IN TOUCH</h3>
+                <form>
+                    <input className='form-input' type="text" placeholder='Name' />
+                    <input className='form-input' type="email" placeholder='Email' required />
+                    <br />
+                    <textarea className='form-input' name="" id="" cols="52" rows="5" placeholder='Message' required></textarea>
+                    <br />
+                    <button className="btn btn-success my-btn">Submit</button>
+                </form>
+            </div>
         </div>
     );
 };
