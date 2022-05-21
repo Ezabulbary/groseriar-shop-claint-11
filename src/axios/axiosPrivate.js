78ac78f1705c87e3a169210bee072f12b8a57f40
Ezabulbary <ezabul.bary@gmail.com>
2022-05-21 15:35:58 +0600

    added my item in items collection

--- src/axios/axiosPrivate.js
+++ src/axios/axiosPrivate.js
@@ -2,12 +2,8 @@ import axios from "axios";
 
 const axiosPrivate = axios.create({});
 
-// Add a request interceptor
 axiosPrivate.interceptors.request.use(function (config) {
     // Do something before request is sent
-    if (!config.headers.authorization) {
-        config.headers.authorization = `Bearer ${localStorage.getItem('accessToken')}`
-    }
     return config;
 }, function (error) {
     // Do something with request error
@@ -22,10 +18,6 @@ axiosPrivate.interceptors.response.use(function (response) {
 }, function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    if (error.response.status === 403) {
-        // refresh token
-        // send to the server
-    }
     return Promise.reject(error);
 });
 
--- src/pages/MyItems/MyItems.js
+++ src/pages/MyItems/MyItems.js
@@ -2,6 +2,7 @@ import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
+import axiosPrivate from '../../axios/axiosPrivate';
 import auth from '../../firebase.init';
 
 const MyItems = () => {
@@ -12,10 +13,10 @@ const MyItems = () => {
     useEffect(() => {
         const getMyItems = async () => {
             const email = user?.email;
-            const url = `http://localhost:5000/myItem?email=${email}`;
+            const url = `http://localhost:5000/items?email=${email}`;
             try {
-                // const { data } = await axiosPrivate.get(url);
-                // setMyItems(data)
+                const { data } = await axiosPrivate.get(url);
+                setMyItems(data)
             }
             catch (error) {
                 console.log(error);
@@ -29,12 +30,49 @@ const MyItems = () => {
         getMyItems()
     }, [user]);
 
+    const deleteItems = id => {
+        const proceed = window.confirm('Are you sure?');
+        if (proceed) {
+            const url = `http://localhost:5000/items/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    const remaining = myItems.filter(item => item._id !== id)
+                    setMyItems(remaining);
+                });
+        }
+    }
+
     return (
-        <div className='w-50 mx-auto text-center'>
-            <h2>This is my orders: {myItems.length}</h2>
+        <div className='container mx-auto text-center'>
+            <h2 className='my-4'>My All Items</h2>
             {
-                myItems.map(item => <div key={item._id}>
-                    <p>{item.email} : {item.service}</p>
+                myItems.map(item => <div className='table-responsive'>
+                    <table class="table table-success table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Email</th>
+                                <th scope="col">User Name</th>
+                                <th scope="col">Item Name</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">Button</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{user?.email}</td>
+                                <td>{item?.supplier_name}</td>
+                                <th scope="row">{item?.name}</th>
+                                <td>{item?.price}</td>
+                                <td>{item?.quantity}</td>
+                                <td><button onClick={() => deleteItems(item._id)} className='rounded'>Delete</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>)
             }
         </div>
